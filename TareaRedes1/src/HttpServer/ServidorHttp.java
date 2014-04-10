5cf14f9bea1582852f71ef804d2d41716412f5d1
Matias <Matias@Matias-PC>
2014-04-10 18:35:12 -0300

    Archivo index de prueba Implementacion del metodo GET Cambio nombres de algunas variables

--- TareaRedes1/src/HttpServer/ServidorHttp.java
+++ TareaRedes1/src/HttpServer/ServidorHttp.java
@@ -6,11 +6,15 @@
 
 package HttpServer;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,7 +26,9 @@ public class ServidorHttp implements Runnable{
     
     //Variables Estaticas
     static final int puerto = 8080;
-    static final String directorio_raiz = ".";
+    static final File directorio_raiz = new File(".");
+    static final String inicio = "index.html";
+    
     Socket conexion;
 
     public ServidorHttp(Socket conexion){
@@ -38,8 +44,8 @@ public class ServidorHttp implements Runnable{
             ServerSocket servidor = new ServerSocket(puerto);
             while(true){
                 ServidorHttp cliente = new ServidorHttp(servidor.accept());
-                Thread hilo = new Thread(cliente);
-                hilo.start();
+                Thread hebra = new Thread(cliente);
+                hebra.start();
             }
         } catch (IOException ex) {
             Logger.getLogger(ServidorHttp.class.getName()).log(Level.SEVERE, null, ex);
@@ -49,16 +55,38 @@ public class ServidorHttp implements Runnable{
 
     @Override
     public void run() {
-        BufferedReader entradacliente = null;
+        // Mensaje cliente, archivo que se pide, metodo POST o GET
+        BufferedReader entradacliente;
+        String archivoPedido;
+        String metodo;
+        BufferedOutputStream salidaArchivo = null;
         try {
+            //Lectura mensaje enviado por el cliente
             entradacliente = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
             String entrada = entradacliente.readLine();
-            System.out.println(entrada);
-            while(entradacliente.readLine() != null){
-                System.out.println(entrada);
-                entrada = entradacliente.readLine();
-            }
+            StringTokenizer token = new StringTokenizer(entrada);
+            metodo = token.nextToken();
+            archivoPedido = token.nextToken();
+            salidaArchivo = new BufferedOutputStream(conexion.getOutputStream());
             
+            if(archivoPedido.equals("/"))
+                archivoPedido += inicio;
+            
+            File archivo = new File(directorio_raiz,archivoPedido);
+            int pesoArchivo = (int) archivo.length();
+            
+            //Implementacion GET
+            if(metodo.equals("GET")){
+                FileInputStream stream;
+                byte[] buffer = new byte[pesoArchivo];
+               
+                stream = new FileInputStream(archivo);
+                stream.read(buffer);
+                
+                salidaArchivo.write(buffer,0,pesoArchivo);
+                salidaArchivo.flush();
+            }
+          
             //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         } catch (IOException ex) {
             Logger.getLogger(ServidorHttp.class.getName()).log(Level.SEVERE, null, ex);
